148f8abaaeace7871a7337c9900ec39cec9a5154
Emma <reyespreciadoemma@gmail.com>
2025-11-05 15:39:17 -0600

    Aplicaación de botones

---
+++ app/src/main/java/com/example/cuartoapractica/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.cuartoapractica
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cuartoapractica.ui.theme.CuartoAPracticaTheme
+import com.example.cuartoapractica.presentation.components.NormalButton
+import com.example.cuartoapractica.presentation.components.NavManager.NavManager
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CuartoAPracticaTheme {
+                NavManager()
+            }
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/example/cuartoapractica/presentation/components/NavManager/NavManager.kt
@@ -0,0 +1,24 @@
+package com.example.cuartoapractica.presentation.components.NavManager
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cuartoapractica.view.DetailsView
+import com.example.cuartoapractica.view.HomeView
+
+@Composable
+fun NavManager(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ){
+        composable("Home") {
+            HomeView(navController)
+        }
+        composable("Details") {
+            DetailsView(navController)
+        }
+    }
+}
